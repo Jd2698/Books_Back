e09c715b2b8d9c869f6d56c4770a8a04a219dd24
DreamCode <dreamcode@MacBook-Pro-de-DreamCode-2.local>
2024-11-18 16:31:43 -0500

    add httpExceptions to loans

--- src/loans/loans.service.ts
+++ src/loans/loans.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -10,18 +15,32 @@ export class LoansService {
   }
 
   async getLoan(loanId: number): Promise<any> {
-    return await this._prismaService.prestamo.findFirst({
-      where: {
-        id: loanId,
-      },
-    });
+    try {
+      const loandFound = await this._prismaService.prestamo.findFirst({
+        where: {
+          id: loanId,
+        },
+      });
+      if (!loandFound) throw new NotFoundException();
+      return loandFound;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
   async createLoan(loanData: any): Promise<any> {
-    const userCreated = await this._prismaService.prestamo.create({
-      data: loanData,
-    });
-
-    return userCreated;
+    try {
+      return await this._prismaService.prestamo.create({
+        data: loanData,
+      });
+    } catch (error) {
+      if (error.code && error.code == 'P2003') {
+        console.log({ message: 'Error foreignkey', code: 'p2003' });
+        throw new ConflictException();
+      }
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
   }
 }
