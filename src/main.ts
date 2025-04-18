96b733d7192dbe91042e9e16ea05971d40b348c6
Jd2698 <jamer.delgado@dreamcodesoft.com>
2025-04-18 13:53:19 -0500

    add supabase connection

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
 	SwaggerModule.setup('api', app, documentFactory)
 
 	app.enableCors({
-		origin: 'http://localhost:4200',
+		origin: ['http://localhost:3000', 'http://localhost:4200'],
 		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
 		credentials: true
 	})
